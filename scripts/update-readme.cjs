8bbd7c306e655d381cfc2ef2a22baf0b0261b772
Jose Familia <128924389+Jose-Familia@users.noreply.github.com>
2024-10-09 13:06:24 -0400

    Update update-readme.cjs

--- scripts/update-readme.cjs
+++ scripts/update-readme.cjs
@@ -20,25 +20,37 @@ async function getLatestProjects() {
     const nonForkRepos = repos.filter(repo => !repo.fork);
     const latestProjects = nonForkRepos.slice(0, 3);
 
-    let projectSection = `## 💼 Experiencia\n\n<div style="display: flex; flex-wrap: wrap; justify-content: space-around;">\n`;
+    let projectSection = `## 💼 Experiencia\n\n`;
 
-    // Generar tarjetas en formato de div
+    // Generar tarjetas en filas de dos columnas
     latestProjects.forEach((repo, index) => {
-        projectSection += `<div style="width: calc(50% - 10px); margin-bottom: 20px;"><a href="${repo.html_url}"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}" alt="${repo.name} card" style="width: 100%; height: auto;" /></a></div>\n`;
-        if ((index + 1) % 2 === 0) {
-            projectSection += `<div style="width: 100%; height: 20px;"></div>\n`;
-        }
+        if (index % 2 === 0) projectSection += `<div>\n`; // Comienza la fila
+        projectSection += `
+            <a href="${repo.html_url}">
+                <img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}" alt="${repo.name} card" />
+            </a>
+        `;
+        if (index % 2 === 1 || index === latestProjects.length - 1) projectSection += `</div>\n`; // Cierra la fila
     });
 
-    projectSection += `<div style="width: calc(50% - 10px); margin-bottom: 20px;"><a href="https://josefamilia.me/"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=portfolio" alt="Portfolio card" style="width: 100%; height: auto;" /></a></div>\n`;
-
-    projectSection += `</div>\n\n`;
+    // Añade el enlace al portafolio como última tarjeta
+    projectSection += `
+        <div>
+            <a href="https://josefamilia.me/">
+                <img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=portfolio" alt="Portfolio card" />
+            </a>
+        </div>
+    `;
 
     let readmeContent = fs.readFileSync('README.md', 'utf8');
     const updatedReadme = readmeContent.replace(/## 💼 Experiencia[\s\S]*?(?=## 📚 Educación)/, projectSection);
 
     fs.writeFileSync('README.md', updatedReadme);
 }
+
+getLatestProjects().catch(error => {
+    console.error('Error updating README:', error);
+});
 
 getLatestProjects().catch(error => {
     console.error('Error updating README:', error);
