a072c996550fcf86bf114d0cf272358719f4afc6
Jose Familia <128924389+Jose-Familia@users.noreply.github.com>
2024-10-09 12:11:05 -0400

    Update update-readme.cjs

--- scripts/update-readme.cjs
+++ scripts/update-readme.cjs
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 
 async function getLatestProjects() {
-    const username = 'Jose-Familia'; // Cambia por tu username de GitHub
+    const username = 'Jose-Familia';
     const url = `https://api.github.com/users/${username}/repos?sort=created&per_page=100`;
 
     const response = await fetch(url, {
@@ -20,20 +20,24 @@ async function getLatestProjects() {
     const nonForkRepos = repos.filter(repo => !repo.fork);
     const latestProjects = nonForkRepos.slice(0, 3);
 
-    let projectSection = `## 📜 Experiencia\n\n`;
+    let projectSection = `## 💼 Experiencia\n\n`;
 
     latestProjects.forEach(repo => {
         projectSection += `[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name})](${repo.html_url})\n\n`;
     });
 
-    // Añadir tarjeta para el portafolio
     projectSection += `[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=portfolio)](https://josefamilia.me/)\n\n`;
 
     let readmeContent = fs.readFileSync('README.md', 'utf8');
-    const updatedReadme = readmeContent.replace(/## 📜 Experiencia[\s\S]*?(?=## 📚 Educación)/, projectSection);
+    const updatedReadme = readmeContent.replace(/## 💼 Experiencia[\s\S]*?(?=## 📚 Educación)/, projectSection);
 
     fs.writeFileSync('README.md', updatedReadme);
 }
+
+getLatestProjects().catch(error => {
+    console.error('Error updating README:', error);
+});
+
 
 getLatestProjects().catch(error => {
     console.error('Error updating README:', error);
