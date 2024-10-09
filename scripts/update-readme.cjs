2f4843ca38671114f9234eb0af2e6174cfd7e724
Jose Familia <128924389+Jose-Familia@users.noreply.github.com>
2024-10-09 12:21:11 -0400

    Update update-readme.cjs

--- scripts/update-readme.cjs
+++ scripts/update-readme.cjs
@@ -20,24 +20,22 @@ async function getLatestProjects() {
     const nonForkRepos = repos.filter(repo => !repo.fork);
     const latestProjects = nonForkRepos.slice(0, 3);
 
-    let projectSection = `## 💼 Experiencia\n\n`;
+    let projectSection = `## 💼 Experiencia\n\n<table>\n<tr>\n`;
 
+    // Generar tarjetas en formato de tabla
     latestProjects.forEach(repo => {
-        projectSection += `[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name})](${repo.html_url})\n\n`;
+        projectSection += `<td align="center"><a href="${repo.html_url}"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}" alt="${repo.name} card" /></a></td>\n`;
     });
 
-    projectSection += `[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=portfolio)](https://josefamilia.me/)\n\n`;
+    projectSection += `<td align="center"><a href="https://josefamilia.me/"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=portfolio" alt="Portfolio card" /></a></td>\n`;
+
+    projectSection += `</tr>\n</table>\n\n`;
 
     let readmeContent = fs.readFileSync('README.md', 'utf8');
     const updatedReadme = readmeContent.replace(/## 💼 Experiencia[\s\S]*?(?=## 📚 Educación)/, projectSection);
 
     fs.writeFileSync('README.md', updatedReadme);
 }
-
-getLatestProjects().catch(error => {
-    console.error('Error updating README:', error);
-});
-
 
 getLatestProjects().catch(error => {
     console.error('Error updating README:', error);
