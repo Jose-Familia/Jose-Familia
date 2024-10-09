eb36663d841aedfcb5f65bd7102989982c7a6038
Jose Familia <128924389+Jose-Familia@users.noreply.github.com>
2024-10-09 12:36:54 -0400

    Update update-readme.cjs

--- scripts/update-readme.cjs
+++ scripts/update-readme.cjs
@@ -20,14 +20,14 @@ async function getLatestProjects() {
     const nonForkRepos = repos.filter(repo => !repo.fork);
     const latestProjects = nonForkRepos.slice(0, 3);
 
-    let projectSection = `## 💼 Experiencia\n\n<div style="display: flex; flex-wrap: wrap; gap: 20px;">\n`;
+    let projectSection = `## 💼 Experiencia\n\n<div style="display: flex; flex-wrap: wrap; justify-content: space-between;">\n`;
 
     // Generar tarjetas en formato de div
     latestProjects.forEach(repo => {
-        projectSection += `<div style="flex: 1 1 calc(50% - 20px);"><a href="${repo.html_url}"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}" alt="${repo.name} card" /></a></div>\n`;
+        projectSection += `<div style="flex: 0 1 calc(48% - 10px); margin-bottom: 20px;"><a href="${repo.html_url}"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}" alt="${repo.name} card" style="width: 100%;" /></a></div>\n`;
     });
 
-    projectSection += `<div style="flex: 1 1 calc(50% - 20px);"><a href="https://josefamilia.me/"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=portfolio" alt="Portfolio card" /></a></div>\n`;
+    projectSection += `<div style="flex: 0 1 calc(48% - 10px); margin-bottom: 20px;"><a href="https://josefamilia.me/"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=portfolio" alt="Portfolio card" style="width: 100%;" /></a></div>\n`;
 
     projectSection += `</div>\n\n`;
 
@@ -36,6 +36,11 @@ async function getLatestProjects() {
 
     fs.writeFileSync('README.md', updatedReadme);
 }
+
+getLatestProjects().catch(error => {
+    console.error('Error updating README:', error);
+});
+
 
 getLatestProjects().catch(error => {
     console.error('Error updating README:', error);
